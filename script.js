2ac7a80002e79a87d221ddb5b7519a68465aa895
Oliver Kesi <oliverkesi@Olivers-MacBook-Pro-2.local>
2019-12-13 13:20:19 +0100

    1. iteration.
    Menu i mobil virker. Hover function i desktop virker ikke.
    Fikses fredag d. 13/12 eller lørdag d. 14/12

--- script.js
+++ script.js
@@ -1,138 +1,206 @@
-
-
-
 document.addEventListener("DOMContentLoaded", start);
 
+const urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get("url");
 
-     let filter = "alle";
-     let indhold;
-     let template = document.querySelector("template");
-     let dest = document.querySelector("#grid");
-     let dest2 = document.querySelector("#press");
-
-
-
-
-     function start() {
-         console.log(start);
-         hentMenu();
-         hentFooter();
-         hentGalleri();
-         hentPresse();
-         const filtrer = document.querySelectorAll(".filter");
-         filtrer.forEach(knap => knap.addEventListener("click", filtrerBilleder));
+let filter = "alle";
+let indhold;
+const template = document.querySelector("template");
+const dest = document.querySelector("#grid");
+const dest2 = document.querySelector("#press");
+const dest3 = document.querySelector("#cv");
 
 
-     }
+function start() {
 
-     async function hentMenu() {
+    hentMenu();
+    hentFooter();
+    hentGalleri();
+    hentPresse();
+    hentCv();
 
-         const voresMenu = await fetch("menu.html");
-         let denHentede = await voresMenu.text();
-         document.querySelector("nav").innerHTML = denHentede;
-         console.log("nav");
-         //            document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-
-     }
+    const filtrer = document.querySelectorAll(".filter");
+    filtrer.forEach(knap => knap.addEventListener("click", filtrerBilleder));
 
 
-     async function hentFooter() {
 
-         const voresMenu = await fetch("footer.html");
-         let denHentede = await voresMenu.text();
-         document.querySelector("footer").innerHTML = denHentede;
-         //            document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
-     }
+}
 
-     async function hentPresse() {
 
-         //Henter vores Json fra WP
-         const url = "http://malikgrosos.com/charlottekrogh/wordpress/wp-json/wp/v2/press?per_page=100"
-         //Henter datafilen
-         const minJson = await fetch(url);
-         //Den hentede data skal tolkes som json
-         indhold = await minJson.json();
-         //Vi kalder funktionen der viser data i DOM
-         visPresse();
-         console.log(hentPresse);
-     }
+async function hentMenu() {
 
+    const voresMenu = await fetch("menu.html");
+    let denHentede = await voresMenu.text();
+    document.querySelector("nav").innerHTML = denHentede;
+    console.log("nav");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+
+async function hentFooter() {
+
+    const voresMenu = await fetch("footer.html");
+    let denHentede = await voresMenu.text();
+    document.querySelector("footer").innerHTML = denHentede;
+
+
+}
+
+async function hentPresse() {
+
+    //Henter vores Json fra WP
+    const url = "http://malikgrosos.com/charlottekrogh/wordpress/wp-json/wp/v2/press?per_page=100"
+    //Henter datafilen
+    const minJson = await fetch(url);
+    //Den hentede data skal tolkes som json
+    indhold = await minJson.json();
+    //Vi kalder funktionen der viser data i DOM
+    visPresse();
+    console.log(hentPresse);
+}
+
+async function hentCv() {
+
+    //Henter vores Json fra WP
+    const url = "http://malikgrosos.com/charlottekrogh/wordpress/wp-json/wp/v2/cv?per_page=100"
+    //Henter datafilen
+    const minJson = await fetch(url);
+    //Den hentede data skal tolkes som json
+    indhold = await minJson.json();
+    //Vi kalder funktionen der viser data i DOM
+    visCv();
+    console.log(hentCv);
+}
 
 async function hentGalleri() {
 
-         //Henter vores Json fra WP
-         const url = "http://malikgrosos.com/charlottekrogh/wordpress/wp-json/wp/v2/vaerk?per_page=100"
-         //Henter datafilen
-         const minJson = await fetch(url);
-         //Den hentede data skal tolkes som json
-         indhold = await minJson.json();
-         //Vi kalder funktionen der viser data i DOM
-         visIndhold();
-         console.log(hentGalleri);
-     }
+    //Henter vores Json fra WP
+    const url = "http://malikgrosos.com/charlottekrogh/wordpress/wp-json/wp/v2/vaerk?per_page=100"
+    //Henter datafilen
+    const minJson = await fetch(url);
+    //Den hentede data skal tolkes som json
+    indhold = await minJson.json();
+    //Vi kalder funktionen der viser data i DOM
+    visIndhold();
+    console.log(hentGalleri);
+}
 
 
-     function filtrerBilleder() {
+function filtrerBilleder() {
 
-         //Sæt variable filter til aktuel værdi
+    //Sæt variable filter til aktuel værdi
 
-         //    let valgt = document.querySelector(".valgt");
+    //    let valgt = document.querySelector(".valgt");
 
-         //    valgt.classList.remove("valgt");
+    //    valgt.classList.remove("valgt");
 
-         filter = this.dataset.filter;
+    filter = this.dataset.filter;
 
-         //    this.classList.add("valgt");
-         console.log(filter);
+    //    this.classList.add("valgt");
 
-         visIndhold();
+    visIndhold();
 
-     }
+}
 
-     function visIndhold() {
-
-         dest.innerHTML = "";
-
-         indhold.forEach(vaerk => {
-             console.log(filter);
-
-             if (vaerk.aar == filter || filter == "alle") { // Filtrere kategorier og kunstnernavn
-
-                 const klon = template.cloneNode(true).content;
-
-                 klon.querySelector("img").src = vaerk.billede.guid;
-//                 klon.querySelector(".aarstal").textContent = vaerk.aar;
+function visIndhold() {
 
 
+    dest.innerHTML = "";
+
+    indhold.forEach(vaerk => {
+        console.log(filter);
 
 
-                 dest.appendChild(klon);
+        if (vaerk.aar == filter || filter == "alle") { // Filtrere kategorier og kunstnernavn
 
-             }
-         })
+            const klon = template.cloneNode(true).content;
 
-
-     }   function visPresse() {
-
-         dest2.innerHTML = "";
-
-         indhold.forEach(vaerk => {
-             console.log(filter);
-
-             if (vaerk.aar == filter || filter == "alle") { // Filtrere kategorier og kunstnernavn
-
-                 const klon = template.cloneNode(true).content;
-
-                 klon.querySelector("img").src = vaerk.article.guid;
-                 klon.querySelector(".titel").textContent = vaerk.titel;
-
-                 dest2.appendChild(klon);
-
-             }
-         })
+            klon.querySelector("img").src = vaerk.billede.guid;
+            klon.querySelector(".aarstal").innerHTML = vaerk.aar;
+            klon.querySelector("img").addEventListener("click", () => {
 
 
-     }
+                visDetalje(vaerk);
 
 
+            })
+
+            dest.appendChild(klon);
+
+        }
+
+
+    })
+}
+
+
+function visPresse() {
+
+    indhold.forEach(vaerk => {
+
+        const klon = template.cloneNode(true).content;
+
+        klon.querySelector("img").src = vaerk.article.guid;
+        klon.querySelector(".titel").textContent = vaerk.titel;
+        klon.querySelector("a").href = vaerk.url;
+
+        dest2.appendChild(klon);
+
+    })
+}
+
+function husetAsnaes(vaerk) {
+    console.log("huset i asnæs")
+
+    location.href = urlLink;
+}
+
+
+function visCv() {
+
+    indhold.forEach(vaerk => {
+
+        const klon = template.cloneNode(true).content;
+
+        klon.querySelector(".cv").innerHTML = vaerk.cv;
+
+        dest3.appendChild(klon);
+
+    })
+}
+
+
+
+function visDetalje(vaerk) {
+    console.log(visDetalje);
+
+
+    location.href = `detalje.html?id=${vaerk.id}`;
+
+}
+
+function toggleMenu() {
+    console.log("click menu");
+
+    document.querySelector(".dropdown-content").classList.toggle("hidden");
+
+
+    let erSkjult = document.querySelector(".dropdown-content").classList.contains("hidden");
+
+    if (erSkjult == true) {
+
+        document.querySelector(".dropdown-content").style.display = "block";
+
+    } else {
+
+        document.querySelector(".dropdown-content").style.display = "none";
+
+    }
+}
+
+
+$(window).load(function () {
+    $(".se-pre-con").fadeOut("slow");;
+});
